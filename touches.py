ca357b71d3a07af8ef76eab776d2e750a7bb7fa5
ToineLey <144327016+ToineLey@users.noreply.github.com>
2025-03-13 09:27:16 +0100

    Update touches.py

--- touches.py
+++ touches.py
@@ -4,13 +4,13 @@ import tty
 import select
 
 
-def getch(timeout=0.05):  # Timeout en secondes
+def getch():  # Timeout en secondes
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
         # Utilisation de select pour attendre l'entrée avec un timeout
-        ready, _, _ = select.select([sys.stdin], [], [], timeout)
+        ready, _, _ = select.select([sys.stdin], [], [], 0.0005)
         if ready:
             return sys.stdin.read(1)  # Si une touche est appuyée
         else:
